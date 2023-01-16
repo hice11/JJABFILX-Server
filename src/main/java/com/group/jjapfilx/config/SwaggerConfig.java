fb3f50ed81bd92389456be94d385f2d142d16d96
CH <hbac11@naver.com>
2023-01-16 23:49:15 +0900

    Swagger Config

---
+++ src/main/java/com/group/jjapfilx/config/SwaggerConfig.java
@@ -0,0 +1,28 @@
+package com.group.jjapfilx.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springdoc.core.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public GroupedOpenApi publicApi() {
+        return GroupedOpenApi.builder()
+                .group("JJAPFLIX")
+                .pathsToMatch("/api/**")
+                .build();
+    }
+
+    @Bean
+    public OpenAPI springShopOpenAPI() {
+        return new OpenAPI()
+                .info(new Info().title("JJAPFLIX API")
+                        .description("넷플릭스 클론 프로젝트")
+                        .version("v0.0.1"));
+    }
+
+}
