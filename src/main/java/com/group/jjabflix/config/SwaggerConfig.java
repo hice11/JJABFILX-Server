32bb277f11e8891f1398a346b12c80249524ca4d
CH <hbac11@naver.com>
2023-01-31 22:00:44 +0900

    chore: Swagger 설정 파일 수정 - header에 Authorize 추가

--- src/main/java/com/group/jjabflix/config/SwaggerConfig.java
+++ src/main/java/com/group/jjabflix/config/SwaggerConfig.java
@@ -1,13 +1,20 @@
 package com.group.jjabflix.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springdoc.core.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.Collections;
+
 @Configuration
 public class SwaggerConfig {
+
+    private static final String API_KEY = "JWT Key";
 
     @Bean
     public GroupedOpenApi publicApi() {
@@ -22,7 +29,14 @@ public class SwaggerConfig {
         return new OpenAPI()
                 .info(new Info().title("JJAPFLIX API")
                         .description("넷플릭스 클론 프로젝트")
-                        .version("v0.0.1"));
+                        .version("v0.0.1"))
+                .components(new Components().addSecuritySchemes(API_KEY, new SecurityScheme()
+                        .name("Authorization")
+                        .type(SecurityScheme.Type.HTTP)
+                        .in(SecurityScheme.In.HEADER)
+                        .scheme("bearer")
+                        .bearerFormat("JWT")))
+                .security(Collections.singletonList(new SecurityRequirement().addList(API_KEY)));
     }
 
 }
