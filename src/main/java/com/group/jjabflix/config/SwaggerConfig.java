f3352f9754728e2bc454832f114a357e04a78e6b
CH <hbac11@naver.com>
2023-02-01 21:30:46 +0900

    build: Swagger 설정 수정 - Authorize 추가

--- src/main/java/com/group/jjabflix/config/SwaggerConfig.java
+++ src/main/java/com/group/jjabflix/config/SwaggerConfig.java
@@ -1,18 +1,25 @@
 package com.group.jjabflix.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springdoc.core.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.Collections;
+
 @Configuration
 public class SwaggerConfig {
+
+    private static final String API_KEY = "JWT Key";
 
     @Bean
     public GroupedOpenApi publicApi() {
         return GroupedOpenApi.builder()
-                .group("JJAPFLIX")
+                .group("JJABFLIX")
                 .pathsToMatch("/api/**")
                 .build();
     }
@@ -20,9 +27,16 @@ public class SwaggerConfig {
     @Bean
     public OpenAPI springShopOpenAPI() {
         return new OpenAPI()
-                .info(new Info().title("JJAPFLIX API")
+                .info(new Info().title("JJABFLIX API")
                         .description("넷플릭스 클론 프로젝트")
-                        .version("v0.0.1"));
+                        .version("v0.0.1"))
+                .components(new Components().addSecuritySchemes(API_KEY, new SecurityScheme()
+                        .name("Authorization")
+                        .type(SecurityScheme.Type.HTTP)
+                        .in(SecurityScheme.In.HEADER)
+                        .scheme("bearer")
+                        .bearerFormat("JWT")))
+                .security(Collections.singletonList(new SecurityRequirement().addList(API_KEY)));
     }
 
 }
